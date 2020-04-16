bc9dd7751c1f21435787c419d43aeff1aa807a8d
Jerry <hall.jerry9@gmail.com>
2020-04-16 13:03:34 -0700

    deploy

---
+++ precache-manifest.baa9f38a1731e3591c2257ae522447fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "460c56d1bd9f49aef64775f3fc1c4752",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61cbd5a4ee5c2b110fb9",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "47932a8f73ce5ab39e10",
+    "url": "./static/js/2.c18fc837.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.c18fc837.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61cbd5a4ee5c2b110fb9",
+    "url": "./static/js/main.dd552af1.chunk.js"
+  },
+  {
+    "revision": "60f07d4d28d685bb5e2f",
+    "url": "./static/js/runtime-main.de288609.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
